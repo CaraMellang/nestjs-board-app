4a22bc9b0d2aac0c843d0d3524f429d0f7dd4680
Mellang <dlwpgh23@naver.com>
2021-10-27 23:27:35 +0900

    add AuthGuard

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      //super는 부모컴포넌트의 것을 사용하는 부분
+      secretOrKey: 'Secret1234',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // 토큰이 어디서 오는지 , 유효한 토큰인지 확인
+    });
+  }
+
+  async validate(payload) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne(username);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
